490a7c010f79856eb607ca48165defd146815594
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2016-03-19 13:30:37 -0400

    create menu, use jade

--- app/scripts/controllers/app.controller.js
+++ app/scripts/controllers/app.controller.js
@@ -1,19 +1,61 @@
 'use strict';
 
 angular.module('kamehameApp')
-  .controller('kamehameCtrl', function($scope, $http) {
+  .controller('kamehameCtrl', function($scope) {
+
     $scope.init = function(){
       $scope.playing = {enabled: true};
       $scope.random = {enabled: true};
       $scope.mute = {enabled:false}
+      $scope.repeat = {enabled:false}
+      $scope.showmenu = {enabled: false}
+
+      $scope.currentClip = 'placeholder'
+
+      $scope.menuControls = [
+        {
+          name: 'play',
+          hover:false,
+          click: function() {$scope.toggle($scope.playing);},
+          label: function(){
+            return $scope.playing.enabled ? 'Pause' : 'Play';
+          }
+        },
+        {
+          name: 'forward',
+          hover:false,
+          click: function() {},
+          label: function(){ return 'Next clip'; }
+        },
+        {
+          name:'backward',
+          hover:false,
+          click: function() {},
+          label: function(){ return 'Previous clip' }
+        },
+        {
+          name: 'repeat',
+          hover:false,
+          click: function() {$scope.toggle($scope.repeat);},
+          label: function(){ return 'Repeat: ' }
+        },
+        {
+          name: 'random',
+          hover:false,
+          click: function() {$scope.toggle($scope.random);},
+          label: function(){ return 'Random: ' }
+        },
+        {
+          name: 'select',
+          hover:false,
+          click: function() {},
+          label: function(){ return 'Select clip...' }
+        }
+      ]
     };
 
-  	$scope.showHideMenu = function() {
-
-  	};
-
-  	$scope.playPause = function(){
-      $scope.playing.enabled = !$scope.playing.enabled;
+  	$scope.toggle = function(state){
+      state.enabled = !state.enabled;
   	};
 
   	$scope.skipForward = function(){
@@ -24,14 +66,6 @@ angular.module('kamehameApp')
 
   	};
 
-  	$scope.toggleRandom = function() {
-      $scope.random.enabled = !$scope.random.enabled;
-  	};
-
-    $scope.toggleMute = function() {
-      $scope.mute.enabled = !$scope.mute.enabled;
-    };
-
   	$scope.getVideo = function(){
 
   	};
@@ -39,6 +73,10 @@ angular.module('kamehameApp')
   	$scope.getVideoList = function(){
 
   	};
+
+    $scope.generateRandomOrder = function() {
+
+    }
 
     $scope.init()
 
--- server.js
+++ server.js
@@ -2,13 +2,33 @@
 var express = require('express');
 var path = require('path');
 var http = require('http')
+var pug = require('pug');
+var fs = require('fs');
 
 var app = express()
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
 
-app.use(express.static(__dirname + '/app'))
+var appRoot = __dirname + '/app'
+
+// compile jade(pug) to html
+jadeFiles = fs.readdirSync(appRoot + '/views')
+for (var j in jadeFiles) {
+	jade = jadeFiles[j]
+	console.log('Compiling %s...', jade);
+	htmlFile = appRoot + '/public/' + jade.replace('.jade', '.html')
+	html = pug.renderFile(appRoot + '/views/' + jade)
+	fs.writeFile(htmlFile, html, function(err) {
+		if (err) {
+			console.log('Error compiling %s: %s', jade, err)
+			process.exit()
+		}
+	})
+}
+
+app.use(express.static(appRoot + '/public'));
+app.use(express.static(appRoot + '/scripts'));
 app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
